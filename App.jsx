3f147ae1440c8e2a4b96d0f09d36095ea077e385
SF_Aquidauana <solofertil2022@gmail.com>
2023-02-10 03:44:07 -0400

    App.jsx modificado

--- App.jsx
+++ App.jsx
@@ -4,8 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar hidden />
     </View>
   );
 }
@@ -13,7 +12,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f4f4f4',
     alignItems: 'center',
     justifyContent: 'center',
   },
