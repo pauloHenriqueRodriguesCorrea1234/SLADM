cb269a26349f92b7799ce9e79925fd0b3fb7164b
SoloFertilAquidauana <solofertil2022@gmail.com>
2023-02-24 19:41:18 -0400

    ..

--- src/services/firebaseAuthentication.js
+++ src/services/firebaseAuthentication.js
@@ -1,16 +1,17 @@
+import { getAuth } from "firebase/auth";
 import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth'
 
-const firebaseConfig = {
+let firebaseConfig = {
   apiKey: "AIzaSyBGmrl68Xj3iXNh_5BMiWHvujN8gpvDMcw",
   authDomain: "solofertiladm-ae735.firebaseapp.com",
   databaseURL: "https://solofertiladm-ae735-default-rtdb.firebaseio.com",
   projectId: "solofertiladm-ae735",
   storageBucket: "solofertiladm-ae735.appspot.com",
   messagingSenderId: "737930469242",
-  appId: "1:737930469242:web:ddd928b117e569250500c3"
+  appId: "1:737930469242:web:ddd928b117e569250500c3",
 };
-
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app)
+const auth = getAuth(app);
+
+export { auth };
