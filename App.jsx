e8d071b1936455ff8c7b08a433002b17181aea8f
Gui7001br <113148079+Gui7001br@users.noreply.github.com>
2023-02-27 17:49:49 -0300

    Criação da SearchBar. Obs: Não foi testado.

--- App.jsx
+++ App.jsx
@@ -1,13 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import Routes from './src/routes';
+import SearchBar from './src/components/SearchBar'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar  barStyle="light-content" />
-      <Routes/>
-    </View>
+
+    <SearchBar />
+    // <View style={styles.container}>
+    //   <StatusBar  barStyle="light-content" />
+    //   <Routes/>
+    // </View>
   );
 }
 
