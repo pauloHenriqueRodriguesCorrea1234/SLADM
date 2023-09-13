b9d71cc6f64d0cd19315da62a61997e9d45e40e1
SoloFertilAquidauana <solofertil2022@gmail.com>
2023-09-12 22:44:54 -0400

    dados paulo 12/09/2023 22:44

--- src/pages/ProducerScreens/Details/index.jsx
+++ src/pages/ProducerScreens/Details/index.jsx
@@ -3,20 +3,30 @@ import { Conteiner, ViewDetails, Image, TextDetails, ViewContact, TextContact, T
 const Details = () => {
   return (
     <Conteiner>
+
       <ViewDetails>
         <Image />
         <TextDetails>Nome:  </TextDetails>
         <TextDetails>Preço: </TextDetails>
       </ViewDetails>
 
-
       <ViewContact>
+        {/* Edit button */}
         <Touchable>
           <TextContact>
+            Editar
+          </TextContact>
+        </Touchable>
 
+        {/* Delete button */}
+        <Touchable>
+          <TextContact>
+            Excluir
           </TextContact>
         </Touchable>
       </ViewContact>
+
+
     </Conteiner>
   )
 }
--- src/pages/ProducerScreens/Details/styles.js
+++ src/pages/ProducerScreens/Details/styles.js
@@ -1,15 +1,37 @@
-import styled from "styled-components";
+import styled from "styled-components"
 
-export const Conteiner = styled.View``
+export const Conteiner = styled.View`
+  flex: 1;
+  background-color: #d3d3d3;
+`
 
-export const ViewDetails = styled.View``
+export const ViewDetails = styled.View`
+  margin: 20px;
+  flex: 0.5;
+`
 
 export const Image = styled.Image``
 
-export const TextDetails = styled.Text``
+export const TextDetails = styled.Text`
+  font-size: 18px;
+  color: #fff;
+`
 
-export const ViewContact = styled.View``
+export const ViewContact = styled.View`
+  flex-direction: row;
+  justify-content: space-around;
+`
 
-export const Touchable = styled.TouchableOpacity``
-          
-export const TextContact = styled.Text``
+export const Touchable = styled.TouchableOpacity`
+  border-width: 1px;
+  padding: 8px;
+  border-radius: 40px;
+  width: 90px;
+  align-items: center;
+  justify-content: center;
+  background-color: #fff;
+`
+
+export const TextContact = styled.Text`
+  font-size: 18px;
+`
